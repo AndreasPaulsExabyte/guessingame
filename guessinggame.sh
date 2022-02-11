b6cf00cf0e04761b8a9f7fdb3c3521f6ed23b7ef
AndreasPaulsExabyte <98325313+AndreasPaulsExabyte@users.noreply.github.com>
2022-02-11 15:39:35 +0100

    identifing numbers and ls command
    added -a to the ls command and used a new if to check for numbers.
    removed stuff.

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,22 @@
 
 function Guess {
 
-echo "plese enter your gues as to which number of files are in thies directory:"
+echo "plese enter your gues as to which number of files are in this directory:"
 read number
 
-local listfiles=$(ls)
-local numberfiles=$(ls | wc -l)
+local re='^[0-9]+$'
+local listfiles=$(ls -a)
+local numberfiles=$(ls -a | wc -l)
 local dis=1
-while [[  $dis == 1  ]] || [[ $dis == 69 ]]
+while [[  $dis == 1  ]]
 do
-	if [[ $number == $numberfiles ]] && [[ $dis = 69 ]]
+	if ! [[ $number =~ $re ]] 
 	then
-		echo "ok now you win and can explayn to me wtf yoou did."
-		let dis=0
-		read explanation
-		echo "so what you say is $explanation i´ll pretend that i´ll do somthing to solve that"
-		exit
-	elif [[ $number == $numberfiles ]]
+   		echo "error: Not a number" >&2;
+   		read number; exit
+	fi
+	
+	if [[ $number == $numberfiles ]]
 	then
 		echo "weee your guess is right"
 		let dis=0
@@ -30,15 +30,10 @@ do
 		echo "$number is to low."
 		read number
 	else
-		echo "i dont know what you did to get here but ok."
-		echo "there are: $listfiles"
-		echo "number is: $numberfiles"
-		echo "now you can type in $numberfile to win"
-		let dis=69
+		echo "it is $numberfile"
 		read number
 	fi
 done
 }
-
 
 Guess
